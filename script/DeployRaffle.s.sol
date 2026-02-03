d8b21bbb722af12fd2918ed6c2b453e48a5f0d6a
Carson Halter <84420466+cah311@users.noreply.github.com>
2026-02-03 07:08:26 -0600

    Deploy Raffle: will deploy contract

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -3,10 +3,28 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
-import {Raffle} from "../src/Raffle.sol";
+import {Raffle} from "src/Raffle.sol";
+import {HelperConffig} from "script/HelperConfig.s.sol";
 
 contract DeployRaffle is Script {
     function run() public {}
 
-    //function deployContract() public returns (Raffle, HelperConfig){}
+    function deployContract() public returns (Raffle, HelperConfig){
+        HelperConfig helperConfig = new HelperConfig();
+        // local -> deploy mocks, get local config
+        // sepolia -> get seploia config
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+
+        vm.startBroadcast();
+        Raffle raffle = new Raffle(
+            config.entranceFee,
+            config.interval,
+            config.vrfCoordinator,
+            config.gasLane,
+            config.subscriptionId,
+            config.callbackGasLimit
+        );
+        vm.stopBroadcast();
+        return (raffle, helperConfig);
+    }
 }
